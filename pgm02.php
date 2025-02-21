52800b5c1a682a1c81aae80783da192406e39867
MahekTodewale <mahektodewle06@gmil.com>
2025-02-21 11:30:21 +0530

    new folder

---
+++ pgm02.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<h1> Login Page</h1>
+<form acton="pgm02.php" method="POST">
+<table>
+<tr><td> Name:</td>
+<td><input type="text" name="Name"/>
+</td></tr>
+<tr><td> password: </td>
+<td> <input type="password" name="password"/> </td>
+</tr>
+<tr> <td colspan="2"> <input type="submit" value="login"/>
+</td> </tr>
+</table>
+</form>
+</body>
+</html>
